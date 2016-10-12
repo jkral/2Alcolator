78bfcab71fbce6a75b000c3610f170e1d75ce88c
jkral <jeff@sandboxbrands.com>
2016-10-12 18:41:49 -0400

    assignment 20

---
+++ 2Alcolator/2Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  2Alcolator
+//
+//  Created by Jeff Kral on 10/12/16.
+//  Copyright © 2016 Jeff Kral. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
